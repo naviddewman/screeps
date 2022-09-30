b8f750fc71ff873ec27ef71fb92571976dd72011
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-09-30 11:49:44 +1000

    update role.fixer.js

--- src/role.fixer.js
+++ src/role.fixer.js
@@ -21,7 +21,7 @@ var roleFixer = {
         if (targets.length > 0 && creep.memory.dumper) {
             if (creep.memory.dumper) {
                 if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0]);
             }
         }
         else if (creep.memory.dumper) {
