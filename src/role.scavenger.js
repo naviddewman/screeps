d752d483c85fc432d55e70f04dc20df4aa2039b5
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-09-30 22:31:26 +1000

    update main.js

---
+++ src/role.scavenger.js
@@ -0,0 +1,28 @@
+require('prototypes.creep.utils');
+
+var roleScavenger = {
+
+    run: function(creep) {
+        const homeRoom = 'EN43N29';
+        const targetRoom = 'EN43N28';
+        const source = Game.getObjectById('5bbcaf829099fc012e63ab04');
+        const storage = Game.getObjectById('63340f12ac9df436b4f8618d');
+
+        if (!creep.isFull() && !creep.memory.dumper) {
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE)
+                creep.moveTo(source);
+        }
+        else if (creep.isFull())
+            creep.memory.dumper = true;
+        
+        if (creep.memory.dumper) {
+            if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                creep.moveTo(storage);
+        }
+
+        if (creep.isEmpty())
+            creep.memory.dumper = false;
+    }
+};
+
+module.exports = roleScavenger;
