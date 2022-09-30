176c72fd1b6586acef1175c673c64c2b42dcb6bd
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-09-30 22:48:02 +1000

    update role.scavenger.js

--- src/role.scavenger.js
+++ src/role.scavenger.js
@@ -4,7 +4,7 @@ var roleScavenger = {
 
     run: function(creep) {
         const homeRoom = 'E43N29';
-        const targetRoom = 'EN43N28';
+        const targetRoom = 'E43N28';
         const source = Game.getObjectById('5bbcaf829099fc012e63ab04');
         const storage = Game.getObjectById('63340f12ac9df436b4f8618d');
 
@@ -12,16 +12,22 @@ var roleScavenger = {
             if (creep.room.name == homeRoom) {
                 creep.moveTo(Game.flags.EN43N28);
             }
-            
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE)
-                creep.moveTo(source);
+            else {
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE)
+                    creep.moveTo(source);
+            }  
         }
         else if (creep.isFull())
             creep.memory.dumper = true;
         
         if (creep.memory.dumper) {
-            if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                creep.moveTo(storage);
+            if(creep.room.name !== homeRoom) {
+                creep.moveTo(Game.flags.Home);
+            }
+            else {
+                if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                    creep.moveTo(storage);
+            }
         }
 
         if (creep.isEmpty())
