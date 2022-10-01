8f13130a3ab9cb58bac337ba0739339b15fa19fb
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-01 17:27:58 +1000

    update role.builder.js

--- src/role.builder.js
+++ src/role.builder.js
@@ -19,8 +19,10 @@ var roleBuilder = {
         
         if (creep.memory.dumper) {
             if (targets.length > 0) {
-                if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
+                let target = creep.pos.findClosestByPath(targets);
+                
+                if (creep.build(targets) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets);
                 }
             }
             else {
