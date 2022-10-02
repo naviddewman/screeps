26471e70e1ef262a620430d45066c4c6e8f33e7b
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-02 22:44:52 +1100

    update role.handler.js

--- src/role.handler.js
+++ src/role.handler.js
@@ -8,6 +8,7 @@ var roleHandler = {
         const storage = Game.getObjectById('63340f12ac9df436b4f8618d');
         
         if (creep.pos.x == 17 && creep.pos.y == 28) {
+            console.log('in if');
             if (!creep.isFull() && !creep.memory.dumper) {
                 creep.withdraw(link, RESOURCE_ENERGY);
             }
