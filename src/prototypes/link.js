641742a29938d5c4838f1559bc8d44fffa0f135e
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-02 22:41:11 +1100

    update link.js

--- src/prototypes/link.js
+++ src/prototypes/link.js
@@ -1,6 +1,6 @@
 StructureLink.prototype.sendTo = 
 function(destination) {
-    
-    if (this.store.getFreeCapacity() == 0)
+
+    if (this.store.getFreeCapacity(RESOURCE_ENERGY) == 0)
         this.transferEnergy(destination);
 };
