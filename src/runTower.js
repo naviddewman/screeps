f708bf9a5ebf332d6b129af43ec4fbbe4486c037
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-03 12:39:33 +1100

    update runTower.js

--- src/runTower.js
+++ src/runTower.js
@@ -4,9 +4,7 @@ require('prototypes.tower');
 var runTower = {
 
     run: function(tower) {
-
-        console.log(tower);
-        const hostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        const hostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (hostile != undefined) { tower.attack(hostile); }
         else {
             tower.repairStructures();
