31b1d3e5db8cc44356482393dd598f1ae24dabe6
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-03 12:13:45 +1100

    update role.combatEngineer.js

--- src/role.combatEngineer.js
+++ src/role.combatEngineer.js
@@ -22,7 +22,7 @@ var roleCombatEngineer = {
         if (creep.memory.dumper) {   
             // maintain tower    
             if (drainedTowers.length > 0) {
-                for (let tower in towers) {
+                for (let tower of towers) {
                     if (creep.transfer(tower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
                         creep.moveTo(tower);
                     }
