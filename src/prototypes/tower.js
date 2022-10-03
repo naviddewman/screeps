09a00e15436dde5ecb05e972352348c2bc308947
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-03 11:57:37 +1100

    update tower.js

--- src/prototypes/tower.js
+++ src/prototypes/tower.js
@@ -4,12 +4,12 @@ StructureTower.prototype.defend =
         if (target != undefined) { this.attack(target); }
     };
 
-StructureTower.prototype.repair =
+StructureTower.prototype.repairStructures =
     function() {
         const target = this.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL && s.structureType != STRUCTURE_RAMPART
         });
-        
+
         if (target) {
             this.repair(target);
         }
