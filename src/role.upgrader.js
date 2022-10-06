a934ae72fc788cb78e8a19a2785e125e2ee1c92a
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-07 09:58:43 +1100

    update role.upgrader.js

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -12,29 +12,57 @@ module.exports = {
     
     
     run: function(creep) {
-        var sources = creep.room.find(FIND_SOURCES);
-        var controller = creep.room.controller;
-        const containers = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER &&
-                structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
+        
+        if (creep.memory.targetRoom != undefined) {
+            const storage = Game.getObjectById('63340f12ac9df436b4f8618d');
+
+            if (!creep.isFull() && !creep.memory.dumper) {
+                creep.energize(storage);
             }
-        });
-     
-        if ((creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) && creep.memory.dumper == false) {
-            creep.moveTo(containers[0]);
+            else if (creep.isFull())
+                creep.memory.dumper = true;
+            
+            if (creep.memory.dumper && creep.room.name != creep.memory.targetRoom) {
+                creep.moveTo(Game.flags.buildInRoom);
+            }
+            else if (creep.memory.dumper && creep.room.name == creep.memory.targetRoom) {
+                const controller = creep.room.controller;
+                if (creep.ungradeController(controller) == ERR_NOT_IN_RANGE)
+                    creep.moveTo(controller);
+            }
+            if (creep.isEmpty())
+                creep.memory.dumper = false;
+
+        }
+        else {
+            const sources = creep.room.find(FIND_SOURCES);
+            const controller = creep.room.controller;
+            const containers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER &&
+                    structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
+                }
+            });
+         
+            if ((creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) && creep.memory.dumper == false) {
+                creep.moveTo(containers[0]);
+            }
+            
+            if (creep.memory.dumper == false && creep.store.getFreeCapacity() == 0)
+                    creep.memory.dumper = true;
+    
+            else if (creep.memory.dumper) {
+                creep.moveTo(controller);
+                creep.upgradeController(controller);
+                
+                if(creep.store.getUsedCapacity() == 0)
+                    creep.memory.dumper = false;
+            }
         }
         
-        if (creep.memory.dumper == false && creep.store.getFreeCapacity() == 0)
-                creep.memory.dumper = true;
-
-        else if (creep.memory.dumper) {
-            creep.moveTo(controller);
-            creep.upgradeController(controller);
-            
-            if(creep.store.getUsedCapacity() == 0)
-                creep.memory.dumper = false;
-        }
+        
+        
+        
     }
 };
 
