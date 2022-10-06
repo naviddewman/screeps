41d18b798a9290323fc9c07d62647b03915b8cc4
David Newman <65800609+naviddewman@users.noreply.github.com>
2022-10-07 09:12:04 +1100

    update role.coloniser.js

--- src/role.coloniser.js
+++ src/role.coloniser.js
@@ -8,23 +8,16 @@ module.exports = {
     ],
 
     run: function(creep) {
-        const exit = creep.room.find(FIND_EXIT_BOTTOM);
         const homeRoom = 'E43N29';
 
-        
-            
-            const controller = creep.room.controller;
-            
             if (creep.room.name = homeRoom) {
-                creep.moveTo(Game.flags.E44N29);
+                creep.moveTo(Game.flags.claimRoom);
             }
-            // else {
+            else {
+                const controller = creep.room.controller;
 
-            // }
-            
-            // if(creep.claimController(controller) == ERR_NOT_IN_RANGE)
-            //     creep.moveTo(controller);
-        
-
+                if(creep.claimController(controller) == ERR_NOT_IN_RANGE)
+                    creep.moveTo(controller);
+            }
     }
 };
